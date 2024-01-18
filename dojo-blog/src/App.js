e54dce4246b87e2b310c3d5977bbbdd810303565
tihomir-a51 <kamenov.tihomir@gmail.com>
2024-01-18 15:49:29 +0200

    adding project

---
+++ dojo-blog/src/App.js
@@ -0,0 +1,23 @@
+import Navbar from './Navbar';
+import Home from './Home';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
+
+function App() {
+
+  return (
+    <div className="App">
+      <Navbar />
+      <div className="content">
+        <Home />
+      </div>
+    </div>
+
+  );
+}
+
+export default App;
+
+
+
+
+
