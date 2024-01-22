7be60661e0e66ed5d3c3d4a73c1f8b5850ef2ec4
tihomir-a51 <kamenov.tihomir@gmail.com>
2024-01-22 23:36:30 +0200

    adding not found router message

--- dojo-blog/src/BlogDetails.js
+++ dojo-blog/src/BlogDetails.js
@@ -3,9 +3,10 @@ import useFetch from "./useFetch";
 import { useHistory } from "react-router-dom";
 
 const BlogDetails = () => {
-    const { id } = useParams()
-    const {data: blog, error, isPending } = useFetch(`http://localhost:8000/blogs/${id}`)
-    const history = useHistory()
+    const { id } = useParams();
+    const {data: blog, error, isPending } = useFetch(`http://localhost:8000/blogs/${id}`);
+    const history = useHistory();
+
     const handleClick = () => {
         fetch('http://localhost:8000/blogs/' + blog.id, {
             method: 'DELETE'
