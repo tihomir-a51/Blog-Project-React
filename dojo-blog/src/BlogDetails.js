ca0e2aa923a103ea0b2cddc210cd78ced4caa79a
tihomir-a51 <kamenov.tihomir@gmail.com>
2024-01-22 22:26:21 +0200

    adding controlled inputs(forms)

--- dojo-blog/src/BlogDetails.js
+++ dojo-blog/src/BlogDetails.js
@@ -3,7 +3,7 @@ import useFetch from "./useFetch";
 
 const BlogDetails = () => {
     const { id } = useParams()
-    const {data: blog, error, isPending } = useFetch("http://localhost:8000/blogs/" + id)
+    const {data: blog, error, isPending } = useFetch(`http://localhost:8000/blogs/${id}`)
     
     return (
         <div className="blog-details">
@@ -15,7 +15,7 @@ const BlogDetails = () => {
                     <p>Written by {blog.author}</p>
                     <div>{blog.body}</div>
                 </article>
-            ) }
+            )}
         </div>
     );
 }
--- dojo-blog/src/Create.js
+++ dojo-blog/src/Create.js
@@ -1,7 +1,45 @@
+import { useState } from "react";
+
 const Create = () => {
+    const [title, setTitle] = useState('');
+    const [body, setBody] = useState('');
+    const [author, setAuthor] = useState('David');
+
+
     return ( 
         <div className="create">
-            <h2>Add a New BLog</h2>
+            <h2>Add a New Blog</h2>
+            <form>
+                <label>Blog Title</label>
+                <input
+                type="text"
+                placeholder="Write your blog body..."
+                required
+                value={title}
+                onChange={(e) => setTitle(e.target.value)}
+                />
+                <label>Blog Body:</label>
+                <textarea 
+                placeholder="Write your blog body..."
+                required
+                value={body}
+                onChange={(e) => setBody(e.target.value)}>
+                </textarea>
+                <label>Blog Author:</label>
+                <select
+                value={author}
+                onChange={(e) => setAuthor(e.target.value)}
+                >
+                    <option value="David">David</option>
+                    <option value="Steven">Steven</option>
+                    <option value="Peter">Peter</option>
+                </select>
+                <button>Add Blog</button>
+                <p>{title}</p>
+                <p>{body}</p>
+                <p>{author}</p>
+
+            </form>
         </div>
      );
 }
